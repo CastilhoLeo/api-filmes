dd13215c212aed2b014ec41913e9eb54f61d878b
LeonardoC <leonardo_castilho1@hotmail.com>
2024-09-09 13:27:29 -0300

    ajustes js

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -2,17 +2,22 @@ const inputPesquisaFilme = document.getElementById("pesquisaFilme");
 const optionList = document.getElementById("opcoesFilmes");
 const resultadoPesquisa = document.getElementById('listaResultadoPesquisa');
 const opcoesFilmes = document.getElementById('opcoesFilmes')
+var filmeSelecionado = 'filmeInicial'
+
+
 
 inputPesquisaFilme.addEventListener('input', function(){
-    if (inputPesquisaFilme.value.length > 2) {
+    if (inputPesquisaFilme.value.length > 2 && !(inputPesquisaFilme.value === filmeSelecionado)) {
+        console.log(!(inputPesquisaFilme.value === filmeSelecionado))
         pesquisaFilme(inputPesquisaFilme.value);
     }
     else{
-        optionList.innerHTML = '';
+        
     }
 });
 
 function pesquisaFilme(filme) {
+    
     fetch(`http://localhost:8080/filme?filme=${filme}`)
     .then(response => response.json())
     .then(data => {
@@ -22,18 +27,23 @@ function pesquisaFilme(filme) {
             option.textContent = filme.title
             option.id = filme.id;
             optionList.appendChild(option);
+
             
         });
     });
+
 }
 
 inputPesquisaFilme.addEventListener('change', function() {
     const selectedValue = inputPesquisaFilme.value;
     const selectedOption = [...optionList.options].find(option => option.textContent === selectedValue);
     
+    
     if (selectedOption) {
         const id = selectedOption.id;
         filmeSugestao(id);
+        
+        
     }
 });
 
